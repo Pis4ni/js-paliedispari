885a8ea192436e015f2441f1e9ec07ae867491d5
Pis4ni <pis4wrk@gmail.com>
2023-07-27 16:44:05 +0200

    add: pari o dispari script

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//chiedo all'utente una parola
+/* //chiedo all'utente una parola
 const parola = prompt('immetti una parola da testare')
 //ribalto la parola
 let reversed =''
@@ -13,11 +13,11 @@ if (reversed == parola) {
 }else{
     //stampo 'non é palindroma'
     console.log('non é palindroma')
-}
+} */
 
 
-// ! FUNCTION VERSION-|
-
+//? FUNCTION VERSION-|
+/* 
 function palOrNot(){
     x = prompt('immetti una parola da testare')
     let reversed =''
@@ -32,6 +32,62 @@ if (reversed == x) {
     console.log('non é palindroma')
     return false 
 }
-}
+} */
 //TODO Un-comment this line to call the function
 //palOrNot()
+
+//!#### EX. 2 ####
+
+/* //salvo il numero scelto dall'utente
+const number = parseInt(prompt('immetti un numero'))
+const select = prompt('scegli "pari" o "dispari"')
+//genero un numero randomico per il pc
+let randomNum = Math.floor(Math.random() * 10)
+//sommo i due numeri insieme e salvo il valore
+let sum = number + randomNum
+//SE la somma dei numeri da un numero pari
+if (sum % 2 == 0) {
+    //dichiaro vincitore chi ha scelto pari
+    console.log('ha vinto pari')
+    if (select == 'pari') {
+        console.log('congratulazioni hai vinto')
+    }else{
+        console.log('spiacente, hai perso')
+    }
+//ALTRIMENTI
+}else{
+    //dichiaro vincitore chi ha scelto dispari
+    console.log('ha vinto dispari')
+    if (select == 'dispari') {
+        console.log('congratulazioni hai vinto')
+    }else{
+        console.log('spiacente, hai perso')
+    }
+} */
+
+
+//? FUNCTION VERSION-|
+
+function evenOddGame(num,selection){
+    let randomNum = Math.floor(Math.random() * 10)
+    let sum = num + randomNum
+//SE la somma dei numeri da un numero pari
+if (sum % 2 == 0) {
+    //dichiaro vincitore chi ha scelto pari
+    console.log('ha vinto pari')
+    if (selection == 'pari') {
+        console.log('congratulazioni hai vinto')
+    }else{
+        console.log('spiacente, hai perso')
+    }
+//ALTRIMENTI
+}else{
+    //dichiaro vincitore chi ha scelto dispari
+    console.log('ha vinto dispari')
+    if (selection == 'dispari') {
+        console.log('congratulazioni hai vinto')
+    }else{
+        console.log('spiacente, hai perso')
+    }
+}
+}
